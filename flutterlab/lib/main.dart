d4909c1c252b7b90ac0f91edf86e283b47b213d8
Darrylwin <logossoudarryl20@gmail.com>
2024-05-05 12:53:53 +0000

    creation du fichier search.dart ('/pages/home/widgets/search.dart')

--- flutterlab/lib/main.dart
+++ flutterlab/lib/main.dart
@@ -190,18 +190,6 @@ Transform(
     width: 200,
   ),
 ),
-Positioned(
-  right: 0,
-  top: 200,
-  child: Transform(
-    transform: Matrix4.identity()..rotateZ(20),
-    origin: const Offset(180, 100),
-    child: Image.asset(
-      'assets/images/bg_liquid.png',
-      width: 200,
-    ),
-  ),
-),
 
 Positioned(
   right: 0,
