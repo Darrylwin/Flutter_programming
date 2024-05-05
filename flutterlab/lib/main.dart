d36f90acc85e4940c9164aa49a102388bc6e0ae3
Darrylwin <logossoudarryl20@gmail.com>
2024-05-05 00:24:30 +0000

    arret: creation de la Searchection (2.4)

--- flutterlab/lib/main.dart
+++ flutterlab/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutterlab/pages/home/widgets/category.dart';
 import 'package:flutterlab/pages/home/widgets/header.dart';
 import 'package:flutterlab/pages/home/widgets/search.dart';
@@ -180,3 +181,45 @@ class HomePage extends StatelessWidget{
     );
   }
 }
+
+Transform(
+  transform: Matrix4.identity()..rotateZ(20),
+  origin: const Offset(150, 50),
+  child: Image.asset(
+    'assets/images/bg_liquid.png',
+    width: 200,
+  ),
+),
+Positioned(
+  right: 0,
+  top: 200,
+  child: Transform(
+    transform: Matrix4.identity()..rotateZ(20),
+    origin: const Offset(180, 100),
+    child: Image.asset(
+      'assets/images/bg_liquid.png',
+      width: 200,
+    ),
+  ),
+),
+
+Positioned(
+  right: 0,
+  top: 200,
+  child: Transform(
+    transform: Matrix4.identity()..rotateZ(20),
+    origin: const Offset(180, 100),
+    child: Image.asset(
+      'assets/images/bg_liquid.png',
+      width: 200,
+    ),
+  ),
+),
+
+Column(
+  children: [
+    const HeaderSection(),
+    const SearchSection(),
+    CategorySection(),
+  ],
+)
