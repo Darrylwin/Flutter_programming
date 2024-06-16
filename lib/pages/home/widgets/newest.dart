0ffcf7cc59314bdc674b9b2c5e5b0613b7a582df
Darrylwin <logossoudarryl20@gmail.com>
2024-06-16 15:13:20 +0000

    Correction des anciennes erreurs. eventuelles erreurs restantes

---
+++ lib/pages/home/widgets/newest.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Container(
+  padding : const EdgeInsets.symmetric(horizontal:25),
+  width: 410,
+  child: const Text(
+    'newest Game',
+    style: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 20,
+    ),
+  ),
+),
+NewestGame(),
